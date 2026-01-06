b8f22cc59961bacb3ce136c48a792ca99adaa4fc
Reno-mathews <renomathews10pro@gmail.com>
2026-01-06 15:22:58 +0530

    Update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,5 +2,17 @@ import { Link } from "react-router-dom";
 
 const posts = [
     { id: 1, title: "First Post", content: "Hello World" },
-    { id: 2, title:}
+    { id: 2, title: "Second Post", content: "Mini blog works!" },
 ];
+
+export default function Home() {
+    return (
+        <div className="max-w-xl mx-auto p-6">
+            <h1 className="text-3xl font-bold mb-4">Mini blog</h1>
+
+            <Link to="/create" className="text-blue-600 underline">
+                Create Post
+            </Link>
+        </div>
+    )
+}
