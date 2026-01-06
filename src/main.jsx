98872aa39362d77c45f537feaee44e7593424fa3
Reno-mathews <renomathews10pro@gmail.com>
2026-01-06 14:18:26 +0530

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -9,3 +9,9 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+  <App />
+  </BrowserRouter>
+);
