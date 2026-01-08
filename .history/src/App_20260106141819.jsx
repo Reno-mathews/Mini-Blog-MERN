3fe96b2e8ff79d363b3acefe078852cd18c14227
Reno-mathews <renomathews10pro@gmail.com>
2026-01-08 10:21:24 +0530

    Update

--- .history/src/App_20260106141819.jsx
+++ .history/src/App_20260106141819.jsx
@@ -1,14 +1,9 @@
 import { Routes, Route} from "react-router-dom";
-import { useState } from "react"
 import Home from "./pages/Home";
 import CreatePost from "./pages/CreatePost";
 import PostDetail from "./pages/PostDetail";
 
 function App() {
-  const [posts, setPosts] = useState([
-    { id: 1, title: "First Post", content: "Hello World"},
-    { id: 2, title: "Second Post", content: "Mini blog works!"},
-  ]);
   return (
     <Routes>
       <Route path="/" element ={<Home />} />
