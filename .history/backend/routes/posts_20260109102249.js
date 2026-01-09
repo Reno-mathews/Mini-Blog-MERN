afa70a1384b847cc5b174e377caa8aa520fe8854
Reno-mathews <renomathews10pro@gmail.com>
2026-01-09 10:22:52 +0530

    Remove env from tracking

---
+++ .history/backend/routes/posts_20260109102249.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const Post = require("../models/Post");
+
+const router = express.Router();
+
+// GET all posts
+router.get("/", async (req,res) => {
+    try {
+        const
+    }
+})
